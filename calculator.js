c1521ee468fb293c0ed6b255bdc1c7fee1c769f2
Alessandro <as-web@hotmail.com>
2023-01-04 15:54:24 +0000

    initial functionality

--- calculator.js
+++ calculator.js
@@ -1,1 +1,25 @@
-console.log("hello");
+// class Calculator {
+//   render() {
+//     // render HTML
+//   }
+// }
+
+// const calculator = new Calculator();
+// calculator.render();
+
+const initCalculator = () => {
+  let currentSequence = [];
+  let buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) =>
+    button.addEventListener("click", (e) => {
+      currentSequence.push(e.target.innerHTML);
+
+      const itemNode = document.createElement("span");
+      itemNode.textContent = e.target.innerHTML;
+      document.getElementById("screen").appendChild(itemNode);
+    })
+  );
+};
+
+document.addEventListener("DOMContentLoaded", initCalculator);
